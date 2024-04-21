31a3a38f98f996e15f1dc9722b28fdb8cf672e24
nahidswe <nahidswe@gmail.com>
2024-04-21 16:08:03 +0600

    backed for chaldal has been created with spring boot

---
+++ src/test/java/com/example/chaldalbackendspringboot/ChaldalBackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chaldalbackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaldalBackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
